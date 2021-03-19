621b768bd2a84fc0170bf51ad4018a257eafa381
Anurag Singh <anurag.r.singh1234@gmail.com>
2021-03-19 23:24:08 +0530

    Basic Working

---
+++ app/src/main/java/com/example/carsavior/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.carsavior;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+//         Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, Anurag!");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Homepage.class);
+                startActivity(intent);
+                finish();
+            }
+        },2500);
+
+    }
+}
