44022422d2d611b0b670605c78dd00f8fa4dc9e3
Aayu231 <aayushi.sarda@gmail.com>
2021-03-20 21:45:43 +0530

    SearchHistory1.0

--- app/src/main/java/com/example/carsavior/Homepage.java
+++ app/src/main/java/com/example/carsavior/Homepage.java
@@ -3,7 +3,9 @@ package com.example.carsavior;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.accounts.Account;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -32,6 +34,7 @@ public class Homepage extends AppCompatActivity {
     ArrayList<String> models = new ArrayList<String>();
     public String fueltype;
     public String model;
+    public String value;
     Button passButton;
 
     @Override
@@ -149,17 +152,83 @@ public class Homepage extends AppCompatActivity {
             public void onNothingSelected(AdapterView<?> parent) {
             }
         });
+        SharedPreferences sharedpreferences = getSharedPreferences("car-savior", Context.MODE_PRIVATE);
+
+        String setting1 = sharedpreferences.getString("car1", "null");
+        if(setting1 != "null"){
+            String[] splitted = setting1.split("\\s+");
+            manufacturer = splitted[0].toLowerCase();
+            value = (splitted[1]+" "+splitted[2]).toLowerCase();
+        }
+        Button scar1 = (Button) findViewById(R.id.button2);
+        scar1.setText(setting1);
+        scar1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("Manufacturer", manufacturer);
+                intent.putExtra("Model", value);
+                Log.i("Done", manufacturer + value);
+
+                startActivity(intent);
+            }
+        });
+        String setting2 = sharedpreferences.getString("car2", "");
+        if(setting2 != "null"){
+            String[] splitted2 = setting2.split("\\s+");
+            manufacturer = splitted2[0].toLowerCase();
+            value = (splitted2[1]+"_"+splitted2[2]).toLowerCase();
+        }
+        Button scar2 = (Button) findViewById(R.id.button3);
+        scar2.setText(setting2);
+        scar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedpreferences = getSharedPreferences("car-savior", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                //String value = model+"_"+fueltype;
+                String temp1 = sharedpreferences.getString("car1", "");
+                String temp2 = sharedpreferences.getString("car2", "");
+                editor.putString("car1", temp2);
+                editor.putString("car2", temp1);
+                Button scar2 = (Button) findViewById(R.id.button3);
+                scar2.setText(temp1);
+                editor.putString("car2", temp1);
+                Button scar1 = (Button) findViewById(R.id.button2);
+                scar1.setText(temp2);
+                editor.apply();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("Manufacturer", manufacturer);
+
+                intent.putExtra("Model", value);
+                Log.i("Done", manufacturer + value);
+
+                startActivity(intent);
+            }
+        });
+
+        //manufacturer + " " + model
 
         passButton = (Button)findViewById(R.id.search_button);
 
         passButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SharedPreferences sharedpreferences = getSharedPreferences("car-savior", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                String value = model+"_"+fueltype;
+                String temp = sharedpreferences.getString("car1", "");
+                editor.putString("car1", manufacturer + " " + model + " " + fueltype);
+                editor.putString("car2", temp);
+                editor.apply();
+
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 intent.putExtra("Manufacturer", manufacturer);
-                String value = model+"_"+fueltype;
+
                 intent.putExtra("Model", value);
                 Log.i("Done", manufacturer + value);
+
 
                 startActivity(intent);
             }
